8bcaa6bec23bc2781211862c0dc8009f234ee23d
botiuk <svetabotiuk@gmail.com>
2024-02-06 10:58:03 +0200

    add I18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module GetWetMantoux
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Supported locales
+    I18n.available_locales = [:uk]
+
+    # Default locale
+    I18n.default_locale = :uk
   end
 end
