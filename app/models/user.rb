2e962d630fee99335f71f50b5f27a86bcb20bce7
botiuk <svetabotiuk@gmail.com>
2024-02-07 13:44:21 +0200

    add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable  
-  
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  after_initialize :set_default_role, if: :new_record?
+
   validates :phone, uniqueness: true, format: { with: /\A[+]?\d+\z/ }, length: { in: 6..15 }
+
+  enum :role, { user: 0, doctor: 1, admin: 2 }
 
   def email_required?
     false
@@ -11,5 +15,9 @@ class User < ApplicationRecord
 
   def email_changed?
     false
+  end
+
+  def set_default_role
+    self.role ||= :user
   end
 end
