8a82016149d4d36116e9a239c0181737227d3836
botiuk <svetabotiuk@gmail.com>
2024-02-27 19:36:56 +0200

    changed enum :doctor_status

--- app/controllers/specialities_controller.rb
+++ app/controllers/specialities_controller.rb
@@ -37,7 +37,7 @@ class SpecialitiesController < ApplicationController
   end
 
   def belonging_doctors
-    @doctors = Doctor.where(speciality_id: params[:id]).where.not(doctor_status: 1)
+    @doctors = Doctor.where(speciality_id: params[:id]).where.not(doctor_status: "fired")
   end
 
   private
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -9,7 +9,7 @@ class Doctor < ApplicationRecord
 
   validates :user_id, uniqueness: true
 
-  enum :doctor_status, { working: 0, fired: 1, vacation: 2 }
+  enum :doctor_status, { working: 0, vacation: 1, fired: 2 }
 
   def self.active_specialities
     Doctor.where.not(doctor_status: "fired").group(:speciality_id).pluck(:speciality_id)
