0981cb6945666380727e54fe7c28114e732eb09f
botiuk <svetabotiuk@gmail.com>
2024-03-04 19:18:30 +0200

    generate users controller with read and update actions, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope '/admin' do
+    resources :users
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
