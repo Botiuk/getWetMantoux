3f993e70d88d48e091744c83f8abe5caf2c8c3b1
botiuk <svetabotiuk@gmail.com>
2024-03-12 13:29:28 +0200

    add factory user, update user_spec

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :user do
+        phone  { Faker::Number.unique.number(digits: 8) }
+        password { "password" }
+        password_confirmation { "password" }    
+    end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,56 +2,56 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe "validations" do
-    user = User.new(phone: "1234567", password: "password")
+    describe "validations" do
 
-    it "is valid with valid attributes" do
-      expect(user).to be_valid
+        it "is valid with valid attributes" do
+            user = build(:user)
+            expect(user).to be_valid
+        end
+
+        it "is not valid without a password" do
+            user = build(:user, password: nil)
+            expect(user).to_not be_valid
+        end
+
+        it "is not valid with too short phone" do
+            user = build(:user, phone: "12345")
+            expect(user).to_not be_valid
+        end
+
+        it "is not valid with too long phone" do
+            user = build(:user, phone: "123456789101112131415")
+            expect(user).to_not be_valid
+        end
+
+        it "is not valid with letters in phone number" do
+            user = build(:user, phone: "123sfdgffg")
+            expect(user).to_not be_valid
+        end
+
+        it "is not valid with not unique phone" do
+            user = create(:user, phone: "123456709878")
+            user_two = build(:user, phone: "123456709878")
+            expect(user_two).to_not be_valid
+        end
     end
 
-    it "is not valid without a password" do
-      user.password = nil
-      expect(user).to_not be_valid
-    end
+    describe "set_default_role" do
+        it "role user if role empty" do
+            user = build(:user)
+            expect(user.role).to eq("user")
+        end
 
-    it "is not valid with too short phone" do
-      user.phone = "12345"
-      expect(user).to_not be_valid
-    end
+        it "role that put when create" do
+            user = build(:user, role: "admin")
+            expect(user.role).to eq("admin")
+        end
 
-    it "is not valid with too long phone" do
-      user.phone = "12345678901234567890"
-      expect(user).to_not be_valid
+        it "role that put when change" do
+            user = build(:user)
+            user.role = "doctor"
+            expect(user.role).to eq("doctor")
+        end
     end
-
-    it "is not valid with letters in phone number" do
-      user.phone = "123klpihj"
-      expect(user).to_not be_valid
-    end
-
-    it "is not valid with not unique phone" do
-      user = User.create!(phone: "1234567", password: "password")
-      user_two = User.new(phone: "1234567", password: "password")
-      expect(user_two).to_not be_valid
-    end
-  end
-
-  describe "set_default_role" do
-    it "role user if role empty" do
-      user = User.create(phone: "1234567", password: "password")
-      expect(user.role).to eq("user")
-    end
-
-    it "role that put when create" do
-      user = User.create(phone: "1234567", password: "password", role: "admin")
-      expect(user.role).to eq("admin")
-    end
-
-    it "role that put when change" do
-      user = User.create(phone: "1234567", password: "password")
-      user.role = "doctor"
-      expect(user.role).to eq("doctor")
-    end
-  end
 
 end
